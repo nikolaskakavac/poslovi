dac375f28b682b8a1cc3bbf6ef43a5600f256913
Skale <tvojemail@example.com>
2026-02-06 20:59:34 +0100

    Add SSL config for Render Postgres

--- backend/config/database.js
+++ backend/config/database.js
@@ -22,12 +22,15 @@ export default {
     logging: false
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    logging: false
+  use_env_variable: 'DATABASE_URL',
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
+}
+  
 };
